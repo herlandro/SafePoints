eea5cc2f9b7dca634092f7aed65b6db77876d32c
Herlandro <herlandro.hermogenes@gmail.com>
2022-03-27 14:31:42 +0100

    Initial commit

---
+++ SafePoints/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SafePoints
+//
+//  Created by Herlandro Hermogenes on 27/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SafePoints/SafePointsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafePointsApp.swift
+//  SafePoints
+//
+//  Created by Herlandro Hermogenes on 27/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SafePointsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
